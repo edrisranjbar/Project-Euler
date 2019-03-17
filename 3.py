04a6762ca91fa4ba623087f5ea0bc91e9e7b4572
edris <edris.qeshm2@gmail.com>
2019-03-17 21:30:26 +0330

    Updated

--- 3.py
+++ 3.py
@@ -1,24 +1,19 @@
 def factorsOf (number):
     factors = []
-    for i in range(1,(number//2)+1):
+    for i in range(1,number//2):
         if number % i == 0 :
             factors.append(i)
     factors.append(number)
-    print ("Factors of {} is/are {}".format(number,factors))
     return factors
 def isPrime(x):
-	if x >= 2:
-		for y in range(2, x):
-			if not (x % y):
-				return False
-	else:
-		return False
+	if x < 2 : return
+	for y in range(2, int(x**0.5) + 1):
+		if not (x % y):
+			return False
 	return True
-number = 100000000
-factors = factorsOf(number)
+factors = factorsOf(60085143)
 primeFactors = []
 for factor in factors:
     if isPrime(factor):
         primeFactors.append(factor)
-print("Prime factors are: " + str(primeFactors))
 print(max(primeFactors))

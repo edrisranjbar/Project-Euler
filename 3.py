761b15666942bfa0be65cd0e48d23fe25c4966a5
edris <34417212+edrisranjbar@users.noreply.github.com>
2019-05-10 02:26:35 +0430

    speed up the program

--- 3.py
+++ 3.py
@@ -1,17 +1,14 @@
-def factorsOf (number):
-    factors = []
-    for i in range(1,number//2):
-        if number % i == 0 :
-            factors.append(i)
-    factors.append(number)
-    return factors
+from functools import reduce
+def factors(n):
+    return set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 def isPrime(x):
 	if x < 2 : return
 	for y in range(2, int(x**0.5) + 1):
 		if not (x % y):
 			return False
 	return True
-factors = factorsOf(60085143)
+factors = factors(600851475143)
 primeFactors = []
 for factor in factors:
     if isPrime(factor):
